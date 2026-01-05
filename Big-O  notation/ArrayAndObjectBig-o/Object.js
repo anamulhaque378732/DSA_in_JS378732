0483509d45868d6bb92b2706c469a983ab66e3e2
Ananul Haque <ananulhaque4631@gmail.com>
2026-01-05 12:50:49 +0600

    End Big-O

---
+++ Big-O  notation/ArrayAndObjectBig-o/Object.js
@@ -0,0 +1,55 @@
+const myObject = {
+    channel: "CodeABC",
+    instructor: "Anamul",
+    videos: [4, 7, 8, 7, 8, 55],
+};
+
+
+// console.log(Object.keys(myObject)); // O(n)
+// console.log(Object.values(myObject)); // O(n)
+// console.log(Object.entries(myObject)); // O(n)
+
+// console.log(myObject.hasOwnProperty("channel")); // O(1)
+
+
+
+
+let myArray = ["Hello", "Sumona", "MyLove", "SweetHeart", "Anamul"];
+
+const newName = myArray.unshift("Madam"); //o(n)
+const perName = myArray.shift()// O(n)
+const nextItem = myArray.push("Raihan"); // O(1)
+const deleteItem = myArray.pop(); // O(1)
+const includes = myArray.includes("Sumona"); //O(n)
+const thirtItem = myArray[2]; // O(1)
+// console.log(thirtItem, includes);
+
+//forEach /map /filter / reduce // o(n)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
