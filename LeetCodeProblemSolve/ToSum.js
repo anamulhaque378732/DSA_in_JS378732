74bbd429f84574fcc7308f2cfdddefd83e562c26
Ananul Haque <ananulhaque4631@gmail.com>
2026-02-07 23:17:13 +0600

    DataStructure

--- LeetCodeProblemSolve/ToSum.js
+++ LeetCodeProblemSolve/ToSum.js
@@ -12,7 +12,7 @@
  * 
  */
 
-
+// Leet code problem 1
 const num = [11, 77, 2, 7, 11, 15, 8];
 const target = 88;
 
@@ -40,7 +40,100 @@ function twoSum(num, target) {
     };
     return false;
 };
-console.log(twoSum([2, 7, 11, 15], 9)); // bigO O(n**2) space bigO O(1)
+// console.log(twoSum([2, 7, 11, 15], 9)); // bigO O(n**2) space bigO O(1)
+
+// leet code problem 11 
+// const heights = [1, 8, 6, 2, 5, 4, 8, 3, 7];
+
+
+const maxAreas = function (heights) {
+    let totalWater = 0;
+    const h = heights.length;
+
+    for (let i = 0; i < h; i++) {
+        for (let j = i + 1; j < h; j++) {
+            // console.log(i, j);
+            let height = Math.min(heights[i], heights[j]);
+
+            // if (heights[i] > heights[j]) {
+            //     // 
+            //     height = heights[j];
+            // } else {
+            //     height = heights[i];
+            // };
+            // console.log(heights[i], heights[j], height);
+
+            let width = j - i;
+            let currentWater = height * width
+
+            // if (totalWater < currentWater) {
+            //     totalWater = currentWater;
+            // }
+
+            totalWater = Math.max(currentWater, totalWater);
+
+
+        };
+    };
+    return totalWater;
+};
+
+// console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7]));
+
+const maxAreaS = function (heights) {
+    let totalWater = 0;
+    const h = heights.length;
+    for (let i = 0; i < h; i++) {
+        for (let j = i + 1; j < h; j++) {
+            let height = Math.min(heights[i], heights[j]);
+            let width = j - i;
+            let currentWater = height * width
+            totalWater = Math.max(currentWater, totalWater);
+        };
+    };
+    return totalWater;
+};
+
+// console.log(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])); O(n^2)
+// console.log(maxArea([1, 1]));
+
+
+
+const maxArea = (heights) => {
+    let totalWater = 0;
+    let firstPointer = 0;
+    let lastPointer = heights.length - 1;
+
+    while (firstPointer < lastPointer) {
+        let height = Math.min(heights[firstPointer], heights[lastPointer])
+        let width = lastPointer - firstPointer;
+        let currentWater = height * width;
+
+        totalWater = Math.max(totalWater, currentWater);
+        if (heights[firstPointer] <= heights[lastPointer]) {
+            firstPointer++;
+        } else {
+            lastPointer--;
+        }
+    }
+    return totalWater;
+
+};
+
+
+console.log(maxArea([1, 8, 6, 12]));
+console.log(maxArea([1, 1]));
+
+
+
+
+
+
+
+
+
+
+
 
 
 
