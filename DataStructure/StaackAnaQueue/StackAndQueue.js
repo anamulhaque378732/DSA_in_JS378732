6a6ba13d73c001cb09d1b7f2d36d0b78c0ca7d02
Ananul Haque <ananulhaque4631@gmail.com>
2026-02-24 16:34:07 +0600

    DataStructure

---
+++ DataStructure/StaackAnaQueue/StackAndQueue.js
@@ -0,0 +1,17 @@
+//  stack
+// principle of stack data structure; : lifo (last in first Out);
+
+
+// Queue data structure
+
+// Principle of Queue data Structure : Fifo(First in first out);
+
+
+
+
+
+
+
+
+
+
